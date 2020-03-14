39572a9aed31079f6817ca0e08b06d5509e38947
jeffdeng1314 <dengh@oregonstate.edu>
2020-03-13 17:16:08 -0700

    Have the UI ready

--- app/src/main/java/com/hfad/bobaway/MainActivity.java
+++ app/src/main/java/com/hfad/bobaway/MainActivity.java
@@ -2,13 +2,22 @@ package com.hfad.bobaway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mWriteReview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_review_page);
+        mWriteReview = findViewById(R.id.et_write_review);
+        InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(mWriteReview.getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
     }
 }
