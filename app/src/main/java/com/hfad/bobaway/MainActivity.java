c2640951074f6a5a5308bf66f0d93cc27bc9a64c
jeffdeng1314 <32289287+jeffdeng1314@users.noreply.github.com>
2020-03-16 15:42:42 -0700

    Added 'dispatchTouchEvent' function which it will hide the soft keyboard when it's not needed

--- app/src/main/java/com/hfad/bobaway/MainActivity.java
+++ app/src/main/java/com/hfad/bobaway/MainActivity.java
@@ -4,20 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText mWriteReview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_review_page);
-        mWriteReview = findViewById(R.id.et_write_review);
-        InputMethodManager inputManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-        inputManager.hideSoftInputFromWindow(mWriteReview.getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+        setContentView(R.layout.activity_main);
+    }
+
+    // This function hides the soft keyboard when click outside of edit text
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
