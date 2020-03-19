68e016f22473edd83a857b7919a6907635576dde
Aeijan <bajracae@oregonstate.edu>
2020-03-19 04:30:37 -0700

    finished the video code, need to edit

--- app/src/main/java/com/hfad/bobaway/ReviewActivity.java
+++ app/src/main/java/com/hfad/bobaway/ReviewActivity.java
@@ -8,13 +8,17 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ReviewActivity extends AppCompatActivity {
     TextView textView;
     EditText editText;
 
-    private static final String DB_URL = "mysql:host=classmysql.engr.oregonstate.edu;dname=cs340_perezjoe";
+    private static final String DB_URL = "jdbc:mysql://192.168.56.1/bobaway";
     private static final String USER = "cs340_perezjoe";
     private static final String PASS = "8666";
 
@@ -27,20 +31,44 @@ public class ReviewActivity extends AppCompatActivity {
         editText = (EditText) findViewById(R.id.et_write_review);
     }
 
-//    public void btnConn(View view) {
-//        Send objectSend = new Send();
-//        objectSend.execute("");
-//    }
+    public void btnConn(View view) {
+        Send objectSend = new Send();
+        objectSend.execute("");
+    }
 
-//    private class Send extends AsyncTask<String, String, String> {
-//        String msg = "";
-//        String text = editText.getText().toString();
-//
-//        @Override
-//        protected void onPreExecute() {
-//            textView.setText("Please Wait Inserting Data");
-//        }
-//
-//
-//    }
+    private class Send extends AsyncTask<String, String, String> {
+        String msg = "";
+        String text = editText.getText().toString();
+
+        @Override
+        protected void onPreExecute() {
+            textView.setText("Please Wait Inserting Data");
+        }
+
+        protected String doInBackground(String... strings) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+                if (conn == null) {
+                    msg = "Connection went wrong";
+                }
+                else{
+                    String query = "INSERT INTO bobaway" + text + ")";
+                    Statement stmt = conn.createStatement();
+                    stmt.executeUpdate(query);
+                    msg = "Inserting Success!";
+                }
+                conn.close();
+            } catch (Exception e) {
+                msg = "Connection went wrong";
+                e.printStackTrace();
+            }
+            return msg;
+        }
+
+        @Override
+        protected void onPostExecute(String msg) {
+            textView.setText(msg);
+        }
+    }
 }
