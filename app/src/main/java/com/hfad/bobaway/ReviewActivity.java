f03c9c118df0632a484c2c6047239cd3b3b5a733
Aeijan <bajracae@oregonstate.edu>
2020-03-19 01:03:21 -0700

    adding ReviewActivity

---
+++ app/src/main/java/com/hfad/bobaway/ReviewActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.bobaway;
+
+import android.os.AsyncTask;
+
+public class ReviewActivity extends AsyncTask {
+
+    protected String doInBackground(String... arg0) {
+        if(byGetOrPost == 0) {
+            try {
+                String username = (String)arg0[0];
+            }
+        }
+    }
+}
